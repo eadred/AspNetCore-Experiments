abe26b08688ff3c16ad6fd9f10c8b32ce099f9b1
Eadred <eadred.birchenough@zuhlke.com>
2017-05-10 15:54:55 +0100

    Hack to check if it is possible to post mujltipart form data with HttpClient

--- src/MvcTest/Controllers/FilesController.cs
+++ src/MvcTest/Controllers/FilesController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Net.Http.Headers;
 using System.IO;
 using MvcTest.Models.Files;
+using System.Net.Http;
 
 namespace MvcTest.Controllers
 {
@@ -67,6 +68,59 @@ namespace MvcTest.Controllers
         private ViewResult IndexView(string message, string downloadFile)
         {
             return View("Index", new FilesViewModel { Message = message, DownloadFile = downloadFile });
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> PostFormData([FromQuery(Name="file")] string filePath)
+        {
+            //See http://stackoverflow.com/questions/16416601/c-sharp-httpclient-4-5-multipart-form-data-upload
+
+            var content = new MultipartFormDataContent();
+
+            var stringContentName = "mystring";
+            var stringContent = new StringContent("Hello World", System.Text.Encoding.UTF8, "text/plain");
+            stringContent.Headers.Add("Content-Disposition", $"form-data; name=\"{stringContentName}\"");
+            content.Add(stringContent, stringContentName);
+
+            var fileContentName = "myfile";
+            var str = System.IO.File.OpenRead(filePath);
+            var fileContent = new StreamContent(str);
+            fileContent.Headers.Add("Content-Type", "image/jpeg");
+            fileContent.Headers.Add("Content-Disposition", $"form-data; name=\"{fileContentName}\"; filename=\"{filePath}\"");
+            content.Add(fileContent, fileContentName, filePath);
+
+            var client = new HttpClient();
+            try
+            {
+                HttpResponseMessage result = await client.PostAsync($"{Request.Scheme}://{Request.Host.Value}/Files/ReceiveFormData", content);
+
+                return StatusCode((int)result.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.ToString());
+            }
+            finally
+            {
+                str.Close();
+                client.Dispose();
+            }
+            
+        }
+
+        [HttpPost]
+        public IActionResult ReceiveFormData([FromForm(Name="mystring")] string thestring, [FromForm(Name = "myfile")] IFormFile thefile)
+        {
+            var filename = thefile.FileName + ".dup";
+
+            using (FileStream fs = System.IO.File.Create(filename))
+            {
+                thefile.CopyTo(fs);
+                fs.Flush();
+            }
+
+            return Ok();
         }
     }
 }
